5edc51d71a13cea9f734ddf902af1d7276672978
yoogg <46551698+yoogg@users.noreply.github.com>
2021-09-12 23:27:58 +0800

    Create installcn.ps1
    winget自动化部署脚本

---
+++ winget/installcn.ps1
@@ -0,0 +1,72 @@
+function inswinget  
+{
+#添加程序集
+Add-Type -AssemblyName System.IO
+Add-Type -AssemblyName System.IO.Compression
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+
+#下载地址
+$DownloadUrl = "https://d.go28.cn/3966850/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
+#下载到Temp目录
+$TempPath = $env:TEMP
+#下载的文件名
+$FileName = "winget.msixbundle"
+#存储的完整文件路径
+$FullPath = "$TempPath\$FileName"
+
+#Download
+$client = New-Object System.Net.WebClient
+"正在下载winget安装包，仅支持win10以上系统"
+$client.DownloadFile($DownloadUrl, $FullPath)
+"下载完成"
+
+#Install
+add-appxpackage $FullPath
+#Invoke-Item $FullPath
+"已运行安装程序，若无法打开请先升级新版win10以上系统"
+}
+
+function wingettool 
+{
+    "---常用命令---"
+    "请输入数字选择功能"
+    "[1]显示已安装软件列表"
+    "[2]升级全部软件"
+    "[3]导出全部软件列表到桌面app.json"
+    "[4]导入全部软件从桌面app.json"
+    "[5]清屏"
+    "[6]退出"
+    $id=Read-Host "请输入数字"
+    if($id -eq 1){winget list
+                wingettool
+                }
+    if($id -eq 2){"正在升级软件"
+            winget upgrade --all    
+            wingettool}
+    if($id -eq 3){"正在导出软件列表"
+                $desktop=[Environment]::GetFolderPath("Desktop")
+                cd $desktop
+                winget export -o app.json
+                wingettool}
+    if($id -eq 4){"正在从桌面app.json文件安装软件，请稍后……"
+                $desktop=[Environment]::GetFolderPath("Desktop")
+                cd $desktop
+                winget import -i app.json
+                "操作完成"
+                wingettool}
+    if($id -eq 5){
+                Clear-Host []
+                wingettool}
+        else{ exit }
+
+}
+
+#判断是否安装winget
+try{
+winget
+"已安装winget,OK"
+wingettool
+}catch{
+    "未安装winget,Not found"
+    inswinget
+}
