87323d55bde9e62aa05740b28b213e771297c135
yoogg <46551698+yoogg@users.noreply.github.com>
2021-09-28 16:25:27 +0800

    添加运行策略

--- script/winapp.ps1
+++ script/winapp.ps1
@@ -1,3 +1,4 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force;
 #下载文件
 function Download($url, $targetFile)
 {
--- winget/installcn.ps1
+++ winget/installcn.ps1
@@ -1,3 +1,4 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force;
 function inswinget  
 {
 #添加程序集
